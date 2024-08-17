6ca1bc9d766878a52851c48010a490e5f727acc4
Tovfikur Rahman <tovfikurrahman@gmail.com>
2024-08-17 14:51:54 +0600

    added header.

--- static/js/nicepage.js
+++ static/js/nicepage.js
@@ -59,3 +59,19 @@ document.addEventListener('DOMContentLoaded', function () {
 
     showStep(currentStep);
 });
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var coll = document.querySelector('.collapsible');
+    var content = document.querySelector('.content');
+
+    coll.addEventListener('click', function() {
+        this.classList.toggle('active');
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+});
